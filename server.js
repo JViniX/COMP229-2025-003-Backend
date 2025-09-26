ac9027478eaac0e521315ca95f514735b35d66df
JViniX <47263194+JViniX@users.noreply.github.com>
2025-09-25 20:31:30 -0400

    Connect

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-const http = require('http');
+const connect = require('connect');
+const app = connect();
 
-http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.end('Hello World');
-}).listen(3000);
+function logger(req, res, next){
+    console.log(req.method, req.url);
+
+    next();
+}
+
+function helloWorld(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World!');
+};
+
+function goodbye(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Goodbye, guys!');
+};
+
+app.use(logger);
+app.use('/hello', helloWorld);
+app.use('/goodbye', goodbye);
+
+app.listen(3000);
 
 console.log('Server running at http://localhost:3000/');
